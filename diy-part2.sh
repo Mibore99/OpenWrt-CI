444d7e477345ed4b49d2113969ad263cfaa06ea8
Mibore99 <854733501@qq.com>
2025-11-27 09:51:03 +0800

    fix

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,1 +1,5 @@
-find feeds/passwall_packages -name "go.mod" -exec sed -i 's/go 1.24/go 1.23/g' {} +
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/openwrt/packages.git -b master --depth 1 temp_packages
+mv temp_packages/lang/golang feeds/packages/lang/
+rm -rf temp_packages
+./scripts/feeds install -a
