c53ce7a669edffaa70713edd6ee8f8e266d3c645
Mibore99 <118157097+Mibore99@users.noreply.github.com>
2026-02-06 09:53:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,3 +1,4 @@
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 ./scripts/feeds install -a
+sed -i 's/download-ci-llvm = true/download-ci-llvm = false/g' package/feeds/packages/rust/Makefile
